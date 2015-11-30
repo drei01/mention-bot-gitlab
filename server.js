38466fea06157cea8c3061781bba86420f4e3249
Matthew Reid <matthew.reid@nakedwines.com>
2015-11-30 16:31:44 +0000

    Fix typo checking for opened merge request

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.post('/', function(req, res) {
   req.pipe(bl(function(err, body) {
     var data = {};
     try { data = JSON.parse(body.toString()); } catch (e) {}
-    if (dataobject_attributes.staten !== 'opened') {
+    if (data.object_attributes.staten !== 'opened') {
       console.log(
         'Skipping because action is ' + data.action + '.',
         'We only care about opened.'
