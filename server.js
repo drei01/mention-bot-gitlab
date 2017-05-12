7d9fcdb3852997133176aa9f81e683a1c5e30a3a
Pascal Borreli <pascal@borreli.com>
2017-05-12 21:07:12 +0100

    Fixed msg

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ app.post('/', function(req, res) {
             request.post({
                 url : process.env.GITLAB_URL + '/api/v3/projects/' + data.object_attributes.target_project_id + '/merge_requests/' + data.object_attributes.id + '/comments',
                 body: JSON.stringify({
-                    body : messageGenerator(
+                    note : messageGenerator(
                       reviewers,
                       buildMentionSentence,
                       defaultMessageGenerator)
